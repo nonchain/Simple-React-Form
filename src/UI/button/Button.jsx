53ca937b58f46195aca7d295671f6fc7225d4bca
progali007 <code.progali@gmail.com>
2022-09-23 17:38:17 +0330

    Designed Button and Input

---
+++ src/UI/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({ text, type, className }) => {
+   return (
+      <button type={type} className={`min-w-[78px] h-14 bg-blue-600 text-white font-bold text-base rounded ${className}`}>{text}</button>
+   )
+}
+
+export default Button
