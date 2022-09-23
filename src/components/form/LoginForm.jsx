99a7725e1a4ebf4a23a6dec33538d1485c8fea7e
progali007 <code.progali@gmail.com>
2022-09-24 00:12:22 +0330

    Set patterns and required state

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -13,8 +13,8 @@ const LoginForm = () => {
    return (
       <form className='w-full flex flex-col' onSubmit={onSubmitHandler}>
          <div className='w-full flex flex-col gap-4'>
-            <Input placeholder='example@gamil.com' type='email' label='Email' inputName='email' id='email' errorMessage='Please enter a valid email'/>
-            <Input placeholder='Enter your password' type='text' label='Password' inputName='password' id='password'/>
+            <Input placeholder='example@gamil.com' type='email' label='Email' inputName='email' id='email' errorMessage='Please enter a valid email' required={true}/>
+            <Input placeholder='Enter your password' type='text' label='Password' inputName='password' id='password' errorMessage='Password must be 8-20 characters and include at least 1 letter' required={true} pattern='^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*]{8,20}$'/>
          </div>
          <Button text='Login' type='submit' className={'mt-20'} />
       </form>
--- src/components/form/RegisterForm.jsx
+++ src/components/form/RegisterForm.jsx
@@ -15,12 +15,12 @@ const RegisterForm = () => {
       <form className='w-full flex flex-col' onSubmit={onSubmitHandler}>
          <div className='w-full flex flex-col gap-4'>
             <div className="flex gap-2 items-center">
-               <Input placeholder='John' type='text' label='Forename' inputName='forename' id='forename' />
-               <Input placeholder='Deco' type='text' label='Surname' inputName='surname' id='surname' /> 
+               <Input placeholder='John' type='text' label='Forename' inputName='forename' id='forename' required={true}/>
+               <Input placeholder='Deco' type='text' label='Surname' inputName='surname' id='surname' required={true}/> 
             </div>
-            <Input placeholder='example@gamil.com' type='email' label='Email' inputName='email' id='email' errorMessage='Please enter a valid email'/>
-            <Input placeholder='At least 3 characters' type='text' label='Username' inputName='username' id='username' errorMessage='Username must contains @'/>
-            <Input placeholder='At least 8 characters' type='text' label='Password' inputName='password' id='password' errorMessage='Password must be at least 8 characters'/>
+            <Input placeholder='example@gamil.com' type='email' label='Email' inputName='email' id='email' errorMessage='Please enter a valid email' required={true}/>
+            <Input placeholder='At least 3 characters' type='text' label='Username' inputName='username' id='username' errorMessage="Username should include '@' and should be 3-16 characters and shouldn't include any special character" required={true} pattern='^[A-Za-z0-9@]{3,16}$'/>
+            <Input placeholder='At least 8 characters' type='text' label='Password' inputName='password' id='password' errorMessage='Password must be 8-20 characters and include at least 1 letter' required={true} pattern='^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*]{8,20}$'/>
          </div>
 
          <Button text='Register' type='submit' className={'mt-20'} />
