583de1e5b571c4226e08509859e2e2017afab0e8
progali007 <code.progali@gmail.com>
2022-09-23 22:00:52 +0330

    Add Login and Register Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import RegisterForm from './components/form/RegisterForm';
+import LoginForm from './components/form/LoginForm';
+
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='w-80 md:w-96 bg-white flex flex-col'>
+      <div className='mb-5 w-full grid grid-cols-2'>
+        <span className='py-5 text-zinc-800 text-xl font-semibold flex items-center justify-center cursor-pointer active-form'>Login</span>
+        <span className='py-5 text-zinc-800 text-xl font-semibold flex items-center justify-center cursor-pointer'>Register</span>
+      </div>
+      <div className='p-6 md:px-8'>
+        <LoginForm />
+        {/* <RegisterForm /> */}
+      </div>
+    </div>
   )
 }
 
