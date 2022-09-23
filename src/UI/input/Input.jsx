f8ac0c4e031854ff9b7db9206404363873c1004e
progali007 <code.progali@gmail.com>
2022-09-24 00:11:31 +0330

    Add pattern and requiret

--- src/UI/input/Input.jsx
+++ src/UI/input/Input.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Input = ({type, id, placeholder, label, errorMessage, inputName}) => {
+const Input = ({type, id, placeholder, label, pattern, required, errorMessage, inputName}) => {
   return (
     <div className='flex flex-col gap-1'>
       <label htmlFor={id} className='text-[17px] font-bold text-zinc-800'>{label}</label>
-      <input type={type} id={id} placeholder={placeholder} name={inputName}
+      <input type={type} id={id} placeholder={placeholder} name={inputName} pattern={pattern} required={required}
       className='px-3 py-2 w-full border-2 border-gray-300 rounded 
-      text-zinc-800 text-sm font-semibold transition-200 focus:border-gray-700 '/>
-      <span className={`text-xs text-red-500 font-semibold mt-1 ${errorMessage ?? 'hidden'}`}>{errorMessage}</span>
+      text-zinc-800 text-sm font-semibold transition-200 focus:border-gray-700 input'/>
+      {errorMessage && <span className={`text-xs text-[#fc4343] font-semibold mt-1 hidden `}>{errorMessage}</span>}
     </div>
   )
 }
