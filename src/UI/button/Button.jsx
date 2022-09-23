81f2a8e1d911a67f1b64d83bfafffc599e2f6475
progali007 <code.progali@gmail.com>
2022-09-23 17:38:37 +0330

    Change height

--- src/UI/button/Button.jsx
+++ src/UI/button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ text, type, className }) => {
    return (
-      <button type={type} className={`min-w-[78px] h-14 bg-blue-600 text-white font-bold text-base rounded ${className}`}>{text}</button>
+      <button type={type} className={`min-w-[78px] h-12 bg-blue-600 text-white font-bold text-base rounded ${className}`}>{text}</button>
    )
 }
 
