b2379cec6f88192c3b4c59c6f15b9cf9d44657b6
progali007 <code.progali@gmail.com>
2022-09-23 17:47:28 +0330

    Change some styles

--- src/UI/input/Input.jsx
+++ src/UI/input/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Input = ({type, id, placeholder, label, refer}) => {
+const Input = ({type, id, placeholder, label, inputName}) => {
   return (
     <div className='flex flex-col gap-1'>
-      <label htmlFor={id} className='text-lg font-bold text-zinc-800'>{label}</label>
-      <input type={type} id={id} placeholder={placeholder} ref={refer}
+      <label htmlFor={id} className='text-[17px] font-bold text-zinc-800'>{label}</label>
+      <input type={type} id={id} placeholder={placeholder} name={inputName}
       className='px-3 py-2 w-full border-2 border-gray-300 rounded 
       text-zinc-800 text-sm font-semibold transition-200 focus:border-gray-700 '/>
     </div>
