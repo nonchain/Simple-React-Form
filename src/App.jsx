e194073958600234f291588cfa31fc618f5e4590
progali007 <code.progali@gmail.com>
2022-09-23 22:13:37 +0330

    Add forms functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterForm from './components/form/RegisterForm';
 import LoginForm from './components/form/LoginForm';
 
 
 function App() {
+  const [showLoginForm, setShowForm] = useState(true);
+
+  const onShowFormHandler = () => {
+    setShowForm(!showLoginForm);
+  }
 
   return (
     <div className='w-80 md:w-96 bg-white flex flex-col'>
       <div className='mb-5 w-full grid grid-cols-2'>
-        <span className='py-5 text-zinc-800 text-xl font-semibold flex items-center justify-center cursor-pointer active-form'>Login</span>
-        <span className='py-5 text-zinc-800 text-xl font-semibold flex items-center justify-center cursor-pointer'>Register</span>
+        <span className={`py-5 text-zinc-800 text-xl font-semibold flex items-center justify-center cursor-pointer ${showLoginForm && 'active-form'}`} onClick={onShowFormHandler}>Login</span>
+        <span className={`py-5 text-zinc-800 text-xl font-semibold flex items-center justify-center cursor-pointer ${!showLoginForm && 'active-form'}`} onClick={onShowFormHandler}>Register</span>
       </div>
       <div className='p-6 md:px-8'>
-        <LoginForm />
-        {/* <RegisterForm /> */}
+        <div className={!showLoginForm && 'hidden'}>
+          <LoginForm />
+        </div>
+        <div className={showLoginForm && 'hidden'}>
+          <RegisterForm />
+        </div>
       </div>
     </div>
   )
