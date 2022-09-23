48343c0775161120a94356d6f13d2c886e67f056
progali007 <code.progali@gmail.com>
2022-09-23 21:58:23 +0330

    Build Login Form

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Button from '../../UI/button/Button';
 import Input from '../../UI/input/Input'
 
 const LoginForm = () => {
-  return (
-    <div className='w-full'>
-      <Input placeholder='username' type='text' label='Username' id='username'/>
-      <Input placeholder='...' type='text' label='Password' id='password'/>
-      
-    </div>
-  )
+   const onSubmitHandler = (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+
+      console.log(Object.fromEntries(formData.entries()));
+   }
+
+   return (
+      <form className='w-full flex flex-col' onSubmit={onSubmitHandler}>
+         <div className='w-full flex flex-col gap-4'>
+            <Input placeholder='example@gamil.com' type='email' label='Email' inputName='email' id='email' />
+            <Input placeholder='Enter your password' type='text' label='Password' inputName='password' id='password' />
+         </div>
+         <Button text='Login' type='submit' className={'mt-20'} />
+      </form>
+   )
 }
 
 export default LoginForm
